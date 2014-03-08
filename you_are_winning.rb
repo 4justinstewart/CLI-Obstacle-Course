0bbe21215606bcdff80b1596614ef12a5e70156c
Justin Stewart <stewart.justin.m@gmail.com>
2014-03-08 17:26:01 -0600

    All commands executed

---
+++ you_are_winning.rb
@@ -0,0 +1,7 @@
+#!/user.bin/env ruby
+puts ""
+puts ""
+puts "your secret instructions are:"
+puts "put all the CLI commands you used for this challenge into a new gist and submit it!"
+puts ""
+puts "hint: try using a bash command to list all previous commands!"
